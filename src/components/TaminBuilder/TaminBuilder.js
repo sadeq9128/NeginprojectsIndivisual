7e97d4fdb9c2af2cee4d92b63e3701fea561e717
Sadeq <sadeq9128@gmail.com>
2021-03-13 08:14:13 +0330

    little change

--- src/components/TaminBuilder/TaminBuilder.js
+++ src/components/TaminBuilder/TaminBuilder.js
@@ -1,19 +1,10 @@
-import React,{ useState, useEffect } from "react";
+import React,{ useState } from "react";
 import TaminForm from "./Tamin/TaminForm";
 import classes from "./TaminBuilder.module.css";
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '100%',
-        '& > * + *': {
-            marginTop: theme.spacing(2),
-        },
-    },
-}));
 
 const TaminBuilder=()=>{
-    const [sending,setSending]=useState(false);
+    const [sending]=useState(false);
 
     const formHandler=(status)=> {
         this.setState({sending:status});
